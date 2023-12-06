8017fda42f02e83a3afd5ab8cd81a029e7947d48
Vinh ODE <78015080+lequocvinh15042001@users.noreply.github.com>
2023-12-06 23:25:18 +0700

    Time: 1 ms (99.88%), Space: 48.5 MB (38.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -8,6 +8,16 @@ class Solution {
             }
             count += (n == result? 1:-1);
         }
+        // for(int i =0; i<nums.length;i++){
+        //     if(count == 0){// Chua tim duoc thi result = nums[i] hien tai
+        //         result = nums[i];
+        //     }
+        //     if(nums[i] == result){
+        //         count++;
+        //     }else {
+        //         count--;
+        //     }
+        // }
         return result;
     }
 }
