9fee12a40b9359c205cba85ae88e495852a9ff4b
Vinh ODE <78015080+lequocvinh15042001@users.noreply.github.com>
2023-12-06 22:19:40 +0700

    Time: 1 ms (97.10%), Space: 43.7 MB (62.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        //nums mang
         
         int swap = 1;
         for(int i = 1; i< nums.length; i++){
             if(nums[i] != nums[i-1]){
                 nums[swap] = nums[i];
-                swap ++;
+                swap++;
             }
         }
-        
         return swap;
         
     }
